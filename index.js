594595448955e2b39c7c7c019ca55f63a702f47d
allan.pirillis <allan.p94@gmail.com>
2021-05-16 12:54:46 -0500

    last changes before merging with develop

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const inquirer = require("inquirer");
 const managerPrompt = require("./src/managerPrompt");
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 
 function init() {
   managerPrompt();
